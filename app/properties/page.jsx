784aa935d17349f622dcc523f6ae6fab36179053
imunreal7 <dubeyaman174@gmail.com>
2025-05-04 12:52:17 +0530

    Added properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,24 @@
-const PropertiesPage = () => {
-    return <div> Properties</div>;
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
+const PropertiesPage = async () => {
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                <h1 className="text-2xl mb-4">Browse Properties</h1>
+                {properties.length === 0 ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <PropertyCard key={property._id} property={property} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default PropertiesPage;
+
