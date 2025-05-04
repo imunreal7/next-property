84daea5bc9b231513403cd65d7c4258c4d3fd0e8
imunreal7 <dubeyaman174@gmail.com>
2025-05-04 09:36:21 +0530

    static files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,13 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+    title: "Next Property",
+    keywords:
+        "rental, property, real estate, buy home, sell home, rent home, buy land, sell land, rent land, buy room, sell room, rent room",
+    description:
+        "Next Property is a real estate platform that allows users to buy and sell properties, as well as rent properties.",
+};
+
 const MainLayout = ({ children }) => {
     return (
         <html>
